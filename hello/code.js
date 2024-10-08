4ae77cbe858e37b2f6f79ea47bca6cbd45c056d7
taylorjones65 <tsizzleblaze36@gmail.com>
2024-10-08 13:37:41 -0500

    Add files via upload

---
+++ hello/code.js
@@ -0,0 +1,7 @@
+
+document.getElementById("b1").addEventListener("click", onclick)
+
+function onclick () {
+    document.getElementById("heading").innerHTML = document.getElementById("data").value 
+       
+}
